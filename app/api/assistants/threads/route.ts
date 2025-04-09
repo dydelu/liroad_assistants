e2fd3977100df3698155fbc472b41dc26353c211
Dylan Delorme Luna <96310474+dydelu@users.noreply.github.com>
2025-04-09 18:31:05 +0200

    Update route.ts

--- app/api/assistants/threads/route.ts
+++ app/api/assistants/threads/route.ts
@@ -2,21 +2,8 @@ import { openai } from "@/app/openai";
 
 export const runtime = "nodejs";
 
-const queryString = window.location.search;
-const urlParams = new URLSearchParams(queryString);
-const vectorStore = urlParams.get('vectorStore');
-//const assistantId = urlParams.get('assistantId');
-
-
 // Create a new thread
 export async function POST() {
-  const thread = await openai.beta.threads.create(
-messages=[ { "role": "user", "content": "How do I cancel my subscription?"} ],
-  tool_resources={
-    "file_search": {
-      "vector_store_ids": [`${vectorStore}`]
-    }
-  }
-  );
+  const thread = await openai.beta.threads.create();
   return Response.json({ threadId: thread.id });
 }
