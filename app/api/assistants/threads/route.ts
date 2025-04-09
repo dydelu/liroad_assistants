f25837336323683cb39e82c78ffcd0ccec10cbd1
Dylan Delorme Luna <96310474+dydelu@users.noreply.github.com>
2025-04-09 18:10:56 +0200

    Update route.ts

--- app/api/assistants/threads/route.ts
+++ app/api/assistants/threads/route.ts
@@ -4,6 +4,12 @@ export const runtime = "nodejs";
 
 // Create a new thread
 export async function POST() {
-  const thread = await openai.beta.threads.create();
+  const thread = await openai.beta.threads.create(
+tool_resources={
+    "file_search": {
+      "vector_store_ids": ["vs_2"]
+    }
+  }
+  );
   return Response.json({ threadId: thread.id });
 }
