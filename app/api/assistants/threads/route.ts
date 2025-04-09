775c2907e17950e047037cb5a4211477b492cfd2
Dylan Delorme Luna <96310474+dydelu@users.noreply.github.com>
2025-04-09 18:23:33 +0200

    Adding URL param

--- app/api/assistants/threads/route.ts
+++ app/api/assistants/threads/route.ts
@@ -2,12 +2,18 @@ import { openai } from "@/app/openai";
 
 export const runtime = "nodejs";
 
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const vectorStore = urlParams.get('vectorStore');
+//const assistantId = urlParams.get('assistantId');
+
+
 // Create a new thread
 export async function POST() {
   const thread = await openai.beta.threads.create(
 tool_resources={
     "file_search": {
-      "vector_store_ids": ["vs_67ef900dd7c48191ababf6da629bb287"]
+      "vector_store_ids": ["${vectorStore}"]
     }
   }
   );
